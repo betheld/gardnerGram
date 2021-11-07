6c3a233d4fb1ba3c0e514c86d7129fc9bac16a68
Bethel D <88989326+betheld@users.noreply.github.com>
2021-11-07 01:20:36 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+	url: 'mongodb+srv://betheld:D3gxSXnXFmeuqCjn@cluster0.ikrip.mongodb.net/gardnerGram?retryWrites=true&w=majority',
+	'dbName': 'gardnerGram',
+};
